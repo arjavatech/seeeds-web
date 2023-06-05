6fadedb5123a5b58176d0d777c77ba907cc5c08a
nithyasriarjava <nithyasri.arjava@gmail.com>
2023-06-05 17:25:10 +0530

    added adimission page

---
+++ admission_script.js
@@ -0,0 +1,14 @@
+let admission2022Btn =document.getElementById('2022Statics');
+let admission2021Btn =document.getElementById('2021Statics');
+let Process2022 =document.getElementById('firstTable');
+let process2021 =document.getElementById('secondTable');
+
+process2021.classList.add('display');
+
+let output2021=()=>{
+//   process2021.classList.remove('display');
+//   Process2022.classList.add('display');
+console.log(`hi`);
+};
+
+admission2021Btn.addEventListener('click',output2021);
