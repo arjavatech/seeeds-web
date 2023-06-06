d5ed83d1a2d91e6517209d258c2a97f0f18cabbd
nithyasriarjava <nithyasri.arjava@gmail.com>
2023-06-06 17:22:00 +0530

    added event page

--- admission_script.js
+++ admission_script.js
@@ -1,14 +1,66 @@
-let admission2022Btn =document.getElementById('2022Statics');
-let admission2021Btn =document.getElementById('2021Statics');
-let Process2022 =document.getElementById('firstTable');
-let process2021 =document.getElementById('secondTable');
+let admission2022Btn = document.getElementById('2022Statics');
+let admission2021Btn = document.getElementById('2021Statics');
+let admission2020Btn = document.getElementById('2020Statics');
+let admission2019Btn = document.getElementById('2019Statics');
+let admission2018Btn = document.getElementById('2018Statics');
 
-process2021.classList.add('display');
+let view2022 = document.getElementById('2022View');
+let view2021 = document.getElementById('2021View');
+let view2020 = document.getElementById('2020View');
+let view2019 = document.getElementById('2019View');
+let view2018 = document.getElementById('2018View');
+let option = document.getElementById('selectedOption');
 
-let output2021=()=>{
-//   process2021.classList.remove('display');
-//   Process2022.classList.add('display');
-console.log(`hi`);
+view2021.classList.add('display');
+view2020.classList.add('display');
+view2019.classList.add('display');
+view2018.classList.add('display');
+
+function admissionProcess() {
+    let getValue = option.value;
+    console.log(getValue);
+    if (getValue === admission2021Btn.value) {
+        view2021.classList.remove('display');
+        view2022.classList.add('display');
+        view2020.classList.add('display');
+        view2019.classList.add('display');
+        view2018.classList.add('display');
+    }
+
+    else if (getValue === admission2022Btn.value) {
+        view2022.classList.remove('display');
+        view2021.classList.add('display');
+        view2020.classList.add('display');
+        view2019.classList.add('display');
+        view2018.classList.add('display');
+        view2017.classList.add('display');
+    }
+
+    else if (getValue === admission2020Btn.value) {
+        view2020.classList.remove('display');
+        view2019.classList.add('display');
+        view2018.classList.add('display');
+        view2021.classList.add('display');
+        view2022.classList.add('display');
+    }
+
+
+    else if (getValue === admission2019Btn.value) {
+        view2019.classList.remove('display');
+        view2020.classList.add('display');
+        view2018.classList.add('display');
+        view2021.classList.add('display');
+        view2022.classList.add('display');
+    }
+
+    else if (getValue === admission2018Btn.value) {
+        view2018.classList.remove('display');
+        view2020.classList.add('display');
+        view2019.classList.add('display');
+        view2021.classList.add('display');
+        view2022.classList.add('display');
+    };
+
+
 };
 
-admission2021Btn.addEventListener('click',output2021);
